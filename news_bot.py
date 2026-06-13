6f298b5c23654763b89e71187af86a996efd8017
Evan Paul <evanpauljacob@gmail.com>
2026-06-13 14:46:14 +0530

    Add news digest box

---
+++ news_bot.py
@@ -0,0 +1,53 @@
+import os
+import feedparser
+import smtplib
+from email.message import EmailMessage
+
+EMAIL = "evanpauljacob27@gmail.com"
+APP_PASSWORD = os.getenv("EMAIL_APP_PASSWORD")
+
+feeds = [
+    ("BBC", "http://feeds.bbci.co.uk/news/rss.xml"),
+    ("Reuters", "https://feeds.reuters.com/reuters/topNews"),
+    ("The Hindu", "https://www.thehindu.com/news/feeder/default.rss")
+]
+
+html = """
+<h1>📰 Daily News Digest</h1>
+"""
+
+for source, url in feeds:
+
+    feed = feedparser.parse(url)
+
+    html += f"<h2>{source}</h2><ul>"
+
+    for item in feed.entries[:3]:
+
+        published = getattr(item, "published", "N/A")
+
+        html += f"""
+        <li>
+            <a href="{item.link}">
+                {item.title}
+            </a>
+            <br>
+            <small>{published}</small>
+        </li>
+        """
+
+    html += "</ul>"
+
+msg = EmailMessage()
+
+msg["Subject"] = "Daily News Digest"
+msg["From"] = EMAIL
+msg["To"] = EMAIL
+
+msg.add_alternative(html, subtype="html")
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+    smtp.login(EMAIL, APP_PASSWORD)
+    smtp.send_message(msg)
+
+print("News email sent!")
